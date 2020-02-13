4729d4d1e41e849e79dd1aadb3228c0717ebf59e
shannonzylstra <shannonzylstra@gmail.com>
2020-02-13 15:02:40 -0800

    Lots of changes, showing background, add sass, add fleur-de-lis

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Nav from '../components/nav'
 import Header from '../components/header'
 import Categories from '../components/categories'
 
 const Home = props => {
     return (
         <div>
+            {/* <Nav /> */}
             <Header />
             <Categories />
         </div>
