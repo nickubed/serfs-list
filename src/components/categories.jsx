9a99185ca83940981d5b2fc6175564dc58645d2a
gtol <gtoledo342@gmail.com>
2020-02-14 11:26:12 -0800

    Styled post

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { GET_LISTINGS } from '../Constants'
+import PostPreview from './subcomponents/PostPreview';
 
-const Categories = props => {
+const Categories = (props) => {
+    const [listings, setListings] = useState([]);
+
+    useEffect(() => {
+        fetch(GET_LISTINGS)
+        .then((response) => {
+            return response.json()
+        })
+        .then((listings) => {
+            setListings(listings)
+        })
+    });
+    const listingElements = listings.map((listing, key) => <PostPreview listing={listing} key={key}/>)
+
     return (
         <div>
-            <div className="category">
-                <p className="catName">Hello</p>
-            </div>
-            <div className="category">
-                <p className="catName">Hello</p>
-            </div>
-            <div className="category">
-                <p className="catName">Hello</p>
-            </div>
+            {listingElements}
         </div>
     )
 }
--- src/pages/post.js
+++ src/pages/post.js
@@ -1,11 +1,62 @@
 import React from 'react'
 import FleurDeLis from '../components/FleurDeLis'
+import ShareIcon from '../components/subcomponents/ShareIcon'
+import ReplyIcon from '../components/subcomponents/ReplyIcon'
 
 
-const Post = () => {
+
+const Post = (props) => {
+    let price
+    if (props.location.state.price) {
+        price =
+            (
+                <div>
+                    <h1>Price</h1>
+                    <span>{props.location.state.price}</span>
+                </div>
+            )
+    } else {
+        price = ''
+    }
     return (
-        <img src={FleurDeLis} alt="favorite" />
+        <div>
+            <div>
+                <div className="post-header">
+                    <span className="post-info">
+                        <span className="post-title">
+                            <span className="post-title-title">{props.location.state.title}</span>                           <button className="button">
+                                <FleurDeLis />
+                                Favorite
+                            </button>
+                        </span>
+                        <h3>Posted {Math.floor(Math.random() * 10)} days ago</h3>
+                    </span>
+                    <div>
+                        <h3>{props.location.state.replyTo}</h3>
+                    </div>
+                </div>
+                <p>{props.location.state.desc}</p>
+                <span className="">
+                    <button className="button">
+                        <ReplyIcon />
+                        Reply
+                    </button>
+                    <button className="button">
+                        <ShareIcon />
+                        Share
+                    </button>
+                </span>
+                <div>
+                    <h3>Category</h3>
+                    <span>Overall > {props.location.state.category} > {props.location.state.subCategory}</span>
+                </div>
+                {price}
+            </div>
+            <div>
+
+            </div>
+        </div>
     )
 }
 
-export default Post
+export default Post;
