901ae183da8d3e54644a4d549a9b4481d49e4949
nickubed <nhpquandt@gmail.com>
2020-02-14 08:56:50 -0800

    added pictures to home page, slight gussying

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -3,14 +3,27 @@ import React from 'react'
 const Categories = props => {
     return (
         <div>
-            <div className="category">
-                <p className="catName">Hello</p>
+            <div>
+                <div className="category" id="community">
+                    <p className="catName">community</p>
+                </div>
+                <div className="category" id="services">
+                    <p className="catName">services</p>
+                </div>
+                <div className="category" id="jobs">
+                    <p className="catName">jobs</p>
+                </div>
             </div>
-            <div className="category">
-                <p className="catName">Hello</p>
-            </div>
-            <div className="category">
-                <p className="catName">Hello</p>
+            <div>
+                <div className="category" id="housing">
+                    <p className="catName">housing</p>
+                </div>
+                <div className="category" id="for_sale">
+                    <p className="catName">for sale</p>
+                </div>
+                <div className="category" id="events">
+                    <p className="catName">events</p>
+                </div>
             </div>
         </div>
     )
